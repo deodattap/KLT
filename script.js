800da4b125574ca74717e5610c9cd395b0f03f3b
Deodatta Pagar <p.deodatta005@gmail.com>
2026-08-09 22:15:27 +0530

    adjusted layout

--- script.js
+++ script.js
@@ -1,20 +1,26 @@
-const shopButton = document.querySelector(".shop");
+const shopButton = document.querySelector(".shop-button");
 
-const learnButton = document.querySelector(".learn");
+const learnButton = document.querySelector(".learn-button");
 
-const contactButton = document.querySelector(".contact");
+const contactButton = document.querySelector(".contact-button");
 
 
 shopButton.addEventListener("click", function () {
+
     alert("Shop Now");
+
 });
 
 
 learnButton.addEventListener("click", function () {
+
     alert("Learn More");
+
 });
 
 
 contactButton.addEventListener("click", function () {
+
     alert("Contact Us");
+
 });
