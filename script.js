0ce485df05f6a206e7148b169a25e81cc77b9727
Deodatta Pagar <p.deodatta005@gmail.com>
2026-08-09 22:11:43 +0530

    initialized structure

---
+++ script.js
@@ -0,0 +1,20 @@
+const shopButton = document.querySelector(".shop");
+
+const learnButton = document.querySelector(".learn");
+
+const contactButton = document.querySelector(".contact");
+
+
+shopButton.addEventListener("click", function () {
+    alert("Shop Now");
+});
+
+
+learnButton.addEventListener("click", function () {
+    alert("Learn More");
+});
+
+
+contactButton.addEventListener("click", function () {
+    alert("Contact Us");
+});
